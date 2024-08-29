03dff17ee923fb622362e7b8a9c26356ed781608
pranay-raj-goud <pranay@convegenius.ai>
2024-08-29 12:46:17 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -4,4 +4,4 @@ st.title("Display Image from GitHub")
 # URL of the image in your GitHub repository
 image_url = "https://raw.githubusercontent.com/pranay-raj-goud/testing2/main/Pic.png"
 # Display the image with a caption
-st.image(image_url, caption="Sample Image from GitHub", use_column_width=True
+st.image(image_url, caption="Sample Image from GitHub", use_column_width=True)
