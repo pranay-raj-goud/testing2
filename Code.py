395a686347636ad35b151377bfbfbb5665bcc90a
pranay-raj-goud <pranay@convegenius.ai>
2024-08-29 12:45:53 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,6 +2,6 @@ import streamlit as st
 # Set the title of the app
 st.title("Display Image from GitHub")
 # URL of the image in your GitHub repository
-image_url = "https://raw.githubusercontent.com/your-username/your-repo/main/images/sample_image.png"
+image_url = "https://raw.githubusercontent.com/pranay-raj-goud/testing2/main/Pic.png"
 # Display the image with a caption
 st.image(image_url, caption="Sample Image from GitHub", use_column_width=True
